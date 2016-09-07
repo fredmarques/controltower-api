aeb278da025efdfabf7d5a1f49f615ecc82d37cf
Frederico Marques <frederico.marques.22@gmail.com>
2016-09-07 18:04:08 -0300

    POST /bots implemented
    (unsafe) without checking database

--- src/aws.js
+++ src/aws.js
@@ -1,4 +1,7 @@
 import ApiBuilder from 'claudia-api-builder';
+import AWS from 'aws-sdk';
+import uuid from 'node-uuid';
+
 const api = new ApiBuilder();
 
 api.post('/v1/customers', req => {
@@ -6,6 +9,38 @@ api.post('/v1/customers', req => {
     console.log('req.body', req.body);
     // const accessToken = req.body.accessToken;
     return 'bar';
+});
+
+// Create a bot
+api.post('/v1/bots', req => {
+    console.log('request.pathParams', JSON.stringify(req.pathParams));
+    console.log('req.body', req.body);
+    const dynamo = new AWS.DynamoDB.DocumentClient();
+
+    const customerId = req.body.customerId;
+    // TODO: check if customerId already existis in data table
+
+    const id = uuid.v4();
+    const params = {
+        TableName: 'ct_bots',
+        Item: {
+            id,
+            customerId
+        }
+    };
+    return dynamo.put(params).promise()
+    .then(() => id)
+    .catch(err => {
+        throw err;
+    });
+});
+
+// Get bot config
+api.post('/v1/bot/{botId}', req => {
+    console.log('request.pathParams', JSON.stringify(req.pathParams));
+    console.log('req.body', req.body);
+    const botId = req.pathParams.botId;
+    return botId;
 });
 
 export default api;
